fa59d0c13dad566d6084e2446d7fa462f44f0705
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2023-11-18 02:43:03 -0300

    Finish enviroment for development

--- broker-routes/app-webhook.js
+++ broker-routes/app-webhook.js
@@ -3,13 +3,19 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+require("dotenv").config();
+const client = require("twilio")(process.env.ACCOUNTSID, AUTHTOKEN);
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.get("/twilio", (req, res) => {
-  return res.json({ get: 200 });
+app.post("/twilio", (req, res) => {
+  return SendMessage();
+});
+
+app.post("/twilio", (req, res) => {
+  return SendMessage();
 });
 
 app.post("/blip", (req, res) => {
@@ -17,10 +23,31 @@ app.post("/blip", (req, res) => {
     statusCode: res.statusCode,
     statusMessage: res.statusMessage,
     headers: res.getHeaders(),
+    body: req.body.user,
   };
 
-  return res.json({ responseData });
+  return res.send;
 });
+
+function SendMessage() {
+  console.log("passei");
+  return new Promise((resolve, reject) => {
+    client.messages
+      .create({
+        body: "TWILIO RESPONSE",
+        from: "whatsapp:+14155238886",
+        to: "whatsapp:+559870221807",
+      })
+      .then((message) => {
+        console.log(message.sid);
+        resolve(message.sid);
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(error);
+      });
+  });
+}
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
