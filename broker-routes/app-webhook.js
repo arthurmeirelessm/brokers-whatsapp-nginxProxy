4a0d8a06f1e9241f7ca1dd29e0c50d3b1e328494
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2023-11-17 22:25:54 -0300

    Up

--- broker-routes/app-webhook.js
+++ broker-routes/app-webhook.js
@@ -4,20 +4,15 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 
-// Configurar body-parser para lidar com JSON
 app.use(bodyParser.json());
 
 app.use(cors());
 
-// Rota para lidar com o webhook do Twilio
 app.get("/twilio", (req, res) => {
-  // Exibir os parâmetros recebidos
-
   return res.json({ get: 200 });
 });
 
 app.post("/blip", (req, res) => {
-  // Exibir apenas as informações relevantes de res
   const responseData = {
     statusCode: res.statusCode,
     statusMessage: res.statusMessage,
@@ -27,7 +22,6 @@ app.post("/blip", (req, res) => {
   return res.json({ responseData });
 });
 
-// Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
